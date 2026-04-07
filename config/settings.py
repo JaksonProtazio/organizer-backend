7bd55cff762be9b2c04a7e86289cd5a89f6ffe37
Jakson Protázio <jakson.protazio1@gmail.com>
2026-04-06 22:52:38 -0300

    feat: organiza rotas no swagger

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'arquivo',
-    'tarefa'
+    'tarefa',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Swagger
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tarefa.urls'))
+    path('api/', include('tarefa.urls')),
+
+    # schema (json)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
