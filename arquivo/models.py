2934d19bf8ed77346d4753312c60743b0db6d038
Jakson Protázio <jakson.protazio1@gmail.com>
2026-04-07 18:53:03 -0300

    fix: adiciona related_name na ForeignKey de Arquivo para Tarefa

--- arquivo/models.py
+++ arquivo/models.py
@@ -5,5 +5,5 @@ from tarefa.models import Tarefa
 
 # Create your models here.
 class Arquivo(models.Model):
-    tarefa = models.ForeignKey(Tarefa, on_delete=models.CASCADE)
+    tarefa = models.ForeignKey(Tarefa, on_delete=models.CASCADE, related_name='arquivos')
     arquivo = models.FileField(upload_to="evidencias/")
